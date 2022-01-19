a5f10f28de0e26f616dc0825cdba1221340c1933
Istomina Alena <mynikisalena@gmail.com>
2022-01-19 12:51:42 +0200

    123

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,14 +1,14 @@
-import { SpinnerDotted } from "spinners-react";
+import { SpinnerDotted } from 'spinners-react';
 
 const Spinner = () => {
   return (
     <SpinnerDotted
       style={{
-        color: "rgb(21, 180, 243)",
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
+        color: 'rgb(136, 119, 181)',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
       }}
     />
   );
